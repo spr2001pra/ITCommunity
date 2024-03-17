603de035c7c404a52077f65988b4a26027ddb78c
spr2001pra <expeak.li@qq.com>
2024-03-17 11:52:40 +0800

    检查登录状态

--- src/main/java/com/nowcoder/community/controller/interceptor/LoginRequiredInterceptor.java
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -26,6 +26,11 @@ public class LoginRequiredInterceptor implements HandlerInterceptor {
         if(handler instanceof HandlerMethod){ // 判断要拦截的对象是否是方法(HandlerMethod)
             HandlerMethod handlerMethod = (HandlerMethod) handler; // 强制由object转换成HandlerMethod类型，便于处理
             Method method = handlerMethod.getMethod();// 调用该类型的getMethod()方法获取method对象
+
+            // 问题解决
+            Class<?> targetClass = handlerMethod.getBeanType(); // 获取处理器的类
+            Method realMethod = targetClass.getDeclaredMethod(method.getName(), method.getParameterTypes());// 重新获取方法对象
+
             LoginRequired loginRequired = method.getAnnotation(LoginRequired.class); // 从方法对象上取方法的注解
             if(loginRequired != null && hostHolder.getUser() == null){ // loginRequired != null说明该方法需要登录才能访问
                 response.sendRedirect(request.getContextPath() + "/login"); // 因为是实现接口定义的方法，不能直接像Controller中return redirect，Controller中底层实际上也是这样写的
@@ -40,9 +45,13 @@ public class LoginRequiredInterceptor implements HandlerInterceptor {
 //        if (handler instanceof HandlerMethod) {
 //            HandlerMethod handlerMethod = (HandlerMethod) handler;
 //            Method method = handlerMethod.getMethod();
+//            Class<?> targetClass = handlerMethod.getBeanType(); // 获取处理器的类
+//            Method realMethod = targetClass.getDeclaredMethod(method.getName(), method.getParameterTypes()); // 重新获取方法对象
+//
+//            LoginRequired loginRequired = realMethod.getDeclaredAnnotation(LoginRequired.class);
 ////            MethodInvocation invocation = null;
 ////            Method method = invocation.getThis().getClass().getDeclaredMethod(invocation.getMethod().getName(),invocation.getMethod().getParameterTypes());
-//            LoginRequired loginRequired = method.getDeclaredAnnotation(LoginRequired.class);
+////            LoginRequired loginRequired = method.getDeclaredAnnotation(LoginRequired.class);
 //            if (loginRequired != null && hostHolder.getUser() == null) {
 //                response.sendRedirect(request.getContextPath() + "/login");
 //                return false;
